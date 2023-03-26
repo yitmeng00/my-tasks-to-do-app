12cd54a6186b48136e68f4bed8f0298e3ceee504
Ivan <ivanwong810520@gmail.com>
2023-03-26 19:41:06 +0800

    Implemented Update To-Do

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ function showTasksList() {
 					<div>
 						<i data-id="${
                             task.id
-                        }" id="to-do-update-btn" class="fa fa-pencil-square-o edit" aria-hidden="true"></i>
+                        }" id="to-do-update-btn" class="fa fa-pencil-square-o update" aria-hidden="true"></i>
 						<i data-id="${
                             task.id
                         }" id="to-do-delete-btn" class="fa fa-trash remove" aria-hidden="true"></i>
@@ -46,6 +46,13 @@ function showTasksList() {
 			`;
 
         tasksList.insertAdjacentHTML("beforeend", element);
+    });
+
+    document.querySelectorAll(`li i.update`).forEach((item) => {
+        item.addEventListener("click", (e) => {
+            e.stopPropagation();
+            showUpdateModal(+e.target.dataset.id);
+        });
     });
 }
 
@@ -66,6 +73,50 @@ function addTask(event) {
     localStorage.setItem("tasks", JSON.stringify(list));
     addTaskInput.value = "";
     showTasksList();
+}
+
+function updateTask(id) {
+    const taskText = document.querySelector("#modal-to-do-desc").value;
+
+    if (taskText.trim().length === 0) return;
+    const taskIndex = list.findIndex((t) => t.id == id);
+
+    list[taskIndex].text = taskText;
+    localStorage.setItem("tasks", JSON.stringify(list));
+
+    const modal = document.getElementById("dynamic-modal");
+    modal.style.display = "none";
+    showTasksList();
+}
+
+function showUpdateModal(id) {
+    const taskIndex = list.findIndex((t) => t.id == id);
+    const { text } = list[taskIndex];
+
+    document.querySelector("#dynamic-modal .modal-body #modal-to-do-id").value =
+        id;
+    document.querySelector(
+        "#dynamic-modal .modal-body #modal-to-do-desc"
+    ).value = text.trim();
+    document
+        .querySelector("#modal-action-btn")
+        .addEventListener("click", () => updateTask(+id));
+
+    const modal = document.getElementById("dynamic-modal");
+    var span = document.getElementsByClassName("close-modal-btn")[0];
+    document.getElementById("modal-header-title").innerHTML =
+        "Update To-Do Description";
+    document.getElementById("modal-action-btn").innerHTML = "Confirm";
+    modal.style.display = "block";
+    span.onclick = function () {
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
 }
 
 addTaskForm.addEventListener("submit", addTask);
